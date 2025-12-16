3bbdbd9da3a51c04e6ea0c78ed9c53cb593e077b
Valery Semenenko <gearmobile@gmail.com>
2025-12-16 22:10:51 +0300

    feat: add header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'add',
+    loadComponent: () => import('./components/add-contact/add-contact').then((m) => m.AddContact),
+  },
+  {
+    path: 'delete',
+    loadComponent: () =>
+      import('./components/delete-contact/delete-contact').then((m) => m.DeleteContact),
+  },
+  {
+    path: '',
+    loadComponent: () => import('./components/home/home').then((m) => m.Home),
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./components/home/home').then((m) => m.Home),
+  },
+];
--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'idb-header',
-  imports: [],
+  imports: [MatToolbarModule, RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {
-
-}
+export class Header {}
